059b6d97a2f1815b3c3128c2903a3e3043c5c92c
ErinMarmullakaj <erin.marmullakaj2@gmail.com>
2024-10-29 17:40:58 +0100

    iii

--- index.php
+++ index.php
@@ -1,19 +1,62 @@
 <?php
 
-$y = "shkolla digjitale";
+// $y = "shkolla digjitale";
 
-echo "i love $y 5";
+// echo "i love $y 5";
 
-$y = 5;
-$z = 10;
+// $y = 5;
+// $z = 10;
 
-echo $y + $z;
-echo $y - $z;
-echo $y * $z;
-echo $z / $y;
-echo $z % $y;
+// echo $y + $z;
+// echo $y - $z;
+// echo $y * $z;
+// echo $z / $y;
+// echo $z % $y;
+
+// $a = "shkolla";
+// $b = "digjitale ";
+// $c = $a. $b;
+// echo"$c \n";
+
+// $programing = "programing is not cool";
+// echo str_replace("not","very", $programing);
+
+// $n = "erin marmullakaj ne shkollen digjitale ";
+
+// echo str_word_count($n);
+
+// $teacher = "erini";
+// echo strlen($teacher);
+
+// $school = "digjitale shkolla";
+// echo strrev($school);
+
+// $num = -5;
+// if($num < 0){
+//     echo"num is less than 0";
+// }
+
+// $x = 1;
+
+// while($x <=5){
+//     echo "the number is $x <br>";
+//     $x++ ;
+// }
 
 
+// $x = 1;
+// do{
+//     echo "the number is $x <br>"
+//     $x++ ;
+// }while($x <= 5);
 
+// for($x=0;$x<=10;$x++){
+//     echo "the number is $x<br>";
+// }
+
+$cars = array('audi','bmw','mercedes');
+for each($cars as $value){
+    echo "value";
+}
 
 ?>
